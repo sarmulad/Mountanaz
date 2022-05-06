6d362822d01613c2460167ffbbee183ce574fa6b
Salam muiz olalekan <60536957+sarmulad@users.noreply.github.com>
2022-05-06 16:31:43 +0100

    gfhjfj

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,7 +4,7 @@ import userIcon from '../assets/Icons/Simplified User Interface Icon.svg'
 import Scaleability from '../assets/Icons/Scaleability Icon.svg'
 import Decentralization from '../assets/Icons/Decentralization Icon.svg'
 import Community from '../assets/Icons/Community Icon.svg'
-
+import ReadMore from './ReadMore'
 
 const Features = () => {
     return (
@@ -15,14 +15,15 @@ const Features = () => {
                 <div className='left'>
                     <img src={userIcon}  alt='Simplified User Interface Icon' />
                     <h2>Stake your tokens</h2>
-                    <p>You may earn interest and the chance to vote on critical issues affecting the ecosystem by owning and staking the Mountanaz token. The staking protocol creates liquidity pools, with pools of staked earning incentives divided proportionally among liquidity suppliers or stakers.</p>
+                    <ReadMore text={'You may earn interest and the chance to vote on critical issues affecting the ecosystem by owning and staking the Mountanaz token. The staking protocol creates liquidity pools, with pools of staked earning incentives divided proportionally among liquidity suppliers or stakers.'} />
                 </div>
 
                 {/* right */}
                 <div className='right'>
                     <img src={Scaleability}  alt='Scaleability Icon' />
                     <h2>Scalability</h2>
-                    <p>Most existing DeFi projects hit a scaling roadblock they couldn't escape. Every transaction must be made public and recorded on the blockchain for blockchain technology to work. This restricts the number of transactions that may be completed at any one moment. Mountanaz is built on a blockchain technology that processes numerous transactions quickly.</p>
+                    <ReadMore text={'Most existing DeFi projects hit a scaling roadblock they couldnt escape. Every transaction must be made public and recorded on the blockchain for blockchain technology to work. This restricts the number of transactions that may be completed at any one moment. Mountanaz is built on a blockchain technology that processes numerous transactions quickly'} />
+                    <p></p>
                 </div>
 
             </div>
@@ -32,7 +33,7 @@ const Features = () => {
                 <div className='left'>
                     <img src={Decentralization}  alt='Simplified User Interface Icon' />
                     <h2>Sustainability in Decentralization</h2>
-                    <p>To adequately safeguard the wealth it hosts, the platform would be wholly decentralized both in the short and long term. The platform and the community as a whole are to be open and permissionless. There is a strong emphasis on decentralization and participation. Both technology and community governance methods should allow for practical iteration while avoiding long-term capture by any one party to ensure sustainability.</p>
+                    <ReadMore text={'To adequately safeguard the wealth it hosts, the platform would be wholly decentralized both in the short and long term. The platform and the community as a whole are to be open and permissionless. There is a strong emphasis on decentralization and participation. Both technology and community governance methods should allow for practical iteration while avoiding long-term capture by any one party to ensure sustainability.'} />
                 </div>
 
                 {/* right */}
